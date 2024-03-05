ce8b0953613b9145e50995aa4b4b644807f56898
Stian Myklebostad (Work) <stian.myklebostad@schibsted.com>
2024-03-05 07:40:20 +0100

    Update 4

--- 4-median-of-two-sorted-arrays/main.c
+++ 4-median-of-two-sorted-arrays/main.c
@@ -1,52 +1,58 @@
-#include <stdio.h>
+#include <stdlib.h>
+
+double median(int* nums, int numsSize) {
+  if (numsSize % 2 == 0) {
+    return (double)(nums[(int)(numsSize / 2 - 1)] + nums[(int)(numsSize / 2)]) / 2;
+  } else {
+    return nums[(int)(numsSize / 2)];
+  }
+}
 
 double findMedianSortedArrays(int* nums1, int nums1Size, int* nums2, int nums2Size) {
+  if (nums1Size == 0) {
+    return median(nums2, nums2Size);
+  } else if (nums2Size == 0) {
+    return median(nums1, nums1Size);
+  }
+
   int total_len = nums1Size + nums2Size;
   // Add 1 to len if total is even as we need the two middle numbers in that case.
   int half_len = total_len / 2 + 1;
 
-  printf("len=%i | hlen=%i | off=%i | odd=%i\n", total_len, half_len, (2 - (total_len % 2)), (total_len % 2));
-
   size_t i1 = 0, i2 = 0;
-  int last[2] = {0};
-  int second_to_last[2] = {0};
+  int last = 0, second_to_last = 0;
 
   for (int i = 0; i < half_len; i++) {
-    printf("i=%i\n", i);
-    second_to_last[0] = last[0];
-    second_to_last[1] = last[1];
+    second_to_last = last;
     if (i1 >= nums1Size) {
-      last[0] = 2;
-      last[1] = nums2[i2];
+      last = nums2[i2];
       i2++;
       continue;
     }
     if (i2 >= nums2Size) {
-      last[0] = 1;
-      last[1] = nums1[i1];
+      last = nums1[i1];
       i1++;
       continue;
     }
     if (nums1[i1] < nums2[i2]) {
-      last[0] = 1;
-      last[1] = nums1[i1];
+      last = nums1[i1];
       i1++;
       continue;
     } else {
-      last[0] = 2;
-      last[1] = nums2[i2];
+      last = nums2[i2];
       i2++;
       continue;
     }
   }
 
-  printf("sl=%i | l=%i\n", second_to_last[1], last[1]);
   if (total_len % 2 == 0) {
-    return (double)(second_to_last[1] + last[1]) / 2;
+    return (double)(second_to_last + last) / 2;
   } else {
-    return last[1];
+    return last;
   }
 }
+
+#include <stdio.h>
 
 int main(void) {
   double ans;
