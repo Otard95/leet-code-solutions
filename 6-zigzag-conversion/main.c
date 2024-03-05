f7f8cde226bb562bad56d5e4e0bcaa6ae5cdd4b6
Stian Myklebostad (Work) <stian.myklebostad@schibsted.com>
2024-03-05 15:02:01 +0100

    WIP: 6 zigzag

---
+++ 6-zigzag-conversion/main.c
@@ -0,0 +1,43 @@
+char* convert(char* s, int numRows) {
+  return s;
+}
+
+// abcdefghijklmnop
+// l 5
+// j 8 6-2 4-4
+//
+// a   i
+// b  hj  p
+// c g k o
+// df  ln
+// e   m
+//
+// PAYPALISHIRING
+// l 4
+// j 6 4-2 2-4 6
+//
+// P  I  N
+// A LS IG
+// YA HR
+// P  I
+//
+// PAYPALISHIRING
+// l 3
+// j 4 2-2 4
+//
+// P A H N
+// APLSIIG
+// Y I R
+//
+// PAYPALISH
+// l 2
+// j 2 2
+//
+// PYPLS
+// APAIH
+//
+// PAYPALISH
+// l 1
+// j 1
+//
+// PAYPALISH
