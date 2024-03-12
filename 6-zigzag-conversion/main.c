caf7aba7b0d36783efacea311de46e8647bf1c19
Stian Myklebostad (Work) <stian.myklebostad@schibsted.com>
2024-03-12 07:14:10 +0100

    Complete zigzag

--- 6-zigzag-conversion/main.c
+++ 6-zigzag-conversion/main.c
@@ -1,10 +1,85 @@
+#include <stdlib.h>
+#include <string.h>
+
 char* convert(char* s, int numRows) {
-  return s;
+  // If we're not mesing with lines the string doesn't change
+  if (numRows == 1) return s;
+
+  size_t len = strlen(s);
+  if (numRows > len) return s;
+
+  // abc0
+  // 0123
+
+  char* out = malloc(sizeof(char) * len+1);
+  if (out == NULL) return NULL;
+  out[len] = 0;
+
+  size_t out_i = 0, max_jump = (numRows - 1) * 2, jumps[2] = {0};
+
+  for (size_t l = 0; l < numRows; l++) {
+    jumps[0] = max_jump - (l * 2);
+    jumps[1] = max_jump - jumps[0];
+    if (jumps[0] == 0 || jumps[1] == 0) {
+      jumps[0] = max_jump;
+      jumps[1] = max_jump;
+    }
+
+    size_t jump = 0, i = l;
+
+    while (i < len) {
+      out[out_i++] = s[i];
+      i += jumps[jump];
+      jump = (jump + 1) % 2;
+    }
+  }
+
+  return out;
 }
 
+#include <stdio.h>
+
+int main(void) {
+  char* ans;
+  ans = convert("abcdefghijklmnopqrstuvwxyz", 3);
+  printf("\n%s\n", ans);
+  free(ans);
+  ans = convert("abcdefghijklmnopqrstuvwxyz", 4);
+  printf("\n%s\n", ans);
+  free(ans);
+  ans = convert("abcdefghijklmnopqrstuvwxyz", 5);
+  printf("\n%s\n", ans);
+  free(ans);
+}
+
+/*
+ 1 1
+ 2 2
+ 3 4
+ 4 6
+ 5 8
+ 6 10
+ 7 12
+
+ (x - 1) * 2
+ ---
+ Criteria:
+   x > 1
+*/
+// abcdefghijklmnop
+// l 6
+// j 10 8-2 6-4 4-6 2-8 10
+//
+// a    k
+// b   jl
+// c  i m
+// d h  n
+// eg   o
+// f    p
+//
 // abcdefghijklmnop
 // l 5
-// j 8 6-2 4-4
+// j 8 6-2 4-4 2-6 8
 //
 // a   i
 // b  hj  p
